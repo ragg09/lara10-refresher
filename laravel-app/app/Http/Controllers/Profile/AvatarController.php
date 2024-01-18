3db8f4a4d46155ba88c87a597fdaabf007e77776
ragg09 <54803732+ragg09@users.noreply.github.com>
2024-01-18 16:29:15 +0800

    Added Avatar | Form Request Validation (#2)
    Co-authored-by: ragg09 <raguanyon@gmail.com>

---
+++ laravel-app/app/Http/Controllers/Profile/AvatarController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Profile;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateAvatarRequest;
+use Illuminate\Http\Request;
+
+class AvatarController extends Controller
+{
+    public function update(UpdateAvatarRequest $request)
+    {
+
+        dd($request->all());
+
+        return back()->with('message', 'Avatar is changed');
+    }
+}
